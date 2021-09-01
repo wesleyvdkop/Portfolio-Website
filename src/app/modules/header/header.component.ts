33e1c29dc35ec9334e7228782c0337a5f3e25c34
Wesley van der Kop <mail@wesleyvanderkop.nl>
2021-09-01 17:02:08 +0200

    Added theme switching

--- src/app/modules/header/header.component.ts
+++ src/app/modules/header/header.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Renderer2 } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
+import { Themes } from '../../shared/theme.service';
 
 @Component({
   selector: 'wk-header',
@@ -8,16 +9,27 @@ import { TranslateService } from '@ngx-translate/core';
 })
 export class HeaderComponent {
   public currentLanguage: string;
+  public currentTheme: Themes = 'light';
 
   constructor(
-    private translateService: TranslateService
+    private translateService: TranslateService,
+    private renderer: Renderer2
   ) {
     this.currentLanguage = this.translateService.getDefaultLang();
-    console.log(this.currentLanguage)
   }
 
   public switchLanguage(language: string) {
     this.translateService.use(language);
     this.currentLanguage = language;
+  }
+
+  public switchTheme(theme: Themes) {
+    if ( theme === 'dark') {
+      this.renderer.addClass(document.body, 'theme-dark');
+      this.currentTheme = 'dark';
+    } else {
+      this.renderer.removeClass(document.body, 'theme-dark');
+      this.currentTheme = 'light';
+    }
   }
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -7,5 +7,5 @@ import { NgModule } from '@angular/core';
   ]
 })
 export class SharedModule {
-  
+
 }
